00d768f691a2a78e37fe9aeb137675747f268b53
Volna08 <natik@yandex.ru>
2022-02-27 18:00:51 +0300

    доработала открытие и закрытие поопап с данными в попап без разметки попап

--- scriptpop.js
+++ scriptpop.js
@@ -1,40 +1,29 @@
-// // пробую нарисовать карточку для попапа
+
 function includPop(data) {
-        const item = `
-    <div class="card-img" style="background-image: url(${data.img_link})"></div>;
-    <h3>${data.name}</h3>;
-    <p>${data.age}</p>;
-    <p>${data.description}</p>;`
-    cardPop.firstElementChild.innerHTML= item;
-        }
+    const item = 
+
+popupCatsImg.innerHTML=`
+<div class="card-img" style="background-image: url(${data.img_link})"></div>;`
+popupCatsText.innerHTML=`<h3>${data.name}</h3>
+ <p>${data.age}</p>
+ <p>${data.description}</p>;`
+    }
 
 
 const popupCats = document.querySelector('.popup_type_cats-info')
 const catImages = document.querySelectorAll('.card')
 const closePopupCats = document.querySelector('.popup_close')
+const popupCatsImg = document.querySelector('.popup_container_img')
+const popupCatsText = document.querySelector('.popup_container_text')
 
 function openPopup(popup) {
- const cardPopup = document.querySelector('.card')
-    alert(cardPopup.textContent)  
-        popup.classList.add('popup_opened')
-    popup.firstElementChild.innerHTML = `
-             <div class="information">
-           <h2>${data.name}</h2>
-           <h3>Возраст: ${data.age}}</h3>
-           <p>${data.description}</p>
-       </div>
-    `
+    popup.classList.add('popup_opened')
 }
 
 function closePopup(popup){
-    const popActive = document.querySelector('.popup_opened')
-    if (popActive) {popActive.classList.remove('popup_opened')}
+const popActive = document.querySelector('.popup_opened')
+if (popActive) {popActive.classList.remove('popup_opened')}
 }
-// function closePopup(popup){
-//       popup.classList.remove('popup_opened')
-// }
-//попап не универсальный
-
 
 function handleClickCatImage(){openPopup(popupCats)}
 
@@ -42,9 +31,9 @@ function handleClosePopup(){closePopup(popupCats)}
 
 closePopupCats.addEventListener('click', handleClosePopup);
 
-catImages.forEach(image => {
-    image.addEventListener("click", () => {
-        openPopup(popupCats)})
-})
-// catImages.addEventListener("click", () => {
-//         openPopup(popupCats)}) - открытие только 1 окна
+for (let i = 0; i < catImages.length; i++) {
+    catImages[i].addEventListener("click", function(e) {
+    openPopup(popupCats);
+    includPop(cats[i])
+    })
+}
